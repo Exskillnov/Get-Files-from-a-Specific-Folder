4ed7cf0e2b22b0bdcc64fdf88c9b11d740d318eb
Nico Lopez <leandrolopezhegui@gmail.com>
2021-03-18 11:59:08 -0300

    Code and executable

---
+++ copyPastingData.py
@@ -0,0 +1,31 @@
+import shutil
+import os
+import time
+
+def listFiles (path):
+    filesList = list()
+    listData = os.listdir(path)
+    for data in listData:
+        if os.path.isfile(os.path.join(path, data)):
+            filesList.append(data)
+    return filesList
+
+text = open('paths.txt', 'r') #We'll only read the paths to copy and paste
+paths = text.read().split('|')
+
+pathToCopy = paths[0].strip()
+pathToPaste = paths[1].strip()
+
+filesList = listFiles(pathToCopy)
+
+for data in filesList:
+    shutil.copy2(pathToCopy + '\\' + data, pathToPaste)
+
+text.close()
+
+print('Data Copied Succesfully. Closing executable...')
+
+time.sleep(5)
+
+os.system('exit')
+
